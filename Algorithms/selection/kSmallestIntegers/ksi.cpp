8119ffd09d7cd0bd9adbd83898dd9913c1d35647
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2019-08-12 16:21:56 +0530

    k smallest elements-partition-AC

--- Algorithms/selection/kSmallestIntegers/ksi.cpp
+++ Algorithms/selection/kSmallestIntegers/ksi.cpp
@@ -1,4 +1,4 @@
-//WA
+//AC
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
@@ -24,7 +24,7 @@ void swap(int *a, int* b){
 }
 
 int partition(int arr[], int l, int r){
-	cout<<"l : "<<l<<" r : "<<r<<endl;
+	// cout<<"l : "<<l<<" r : "<<r<<endl;
 	int pivot = arr[r];
 	int j = l-1;
 	for(int i=l; i<r; i++){
@@ -35,30 +35,36 @@ int partition(int arr[], int l, int r){
 	}
 	++j;
 	swap(arr+j, arr+r);
-	return j; //this is the actual positon
+	return j-l+1; //this is the actual positon
 }
 	
 void printKSmallest(int arr[], int l, int r, int k){
+	if(k <= 0)
+		return;
 
 	int pivot = arr[r];
 	int pos = partition(arr, l, r);//this is the actual position (1 based index)
-	cout<<"position : "<<pos<<" k : "<<k<<endl;
+	// cout<<"position : "<<pos<<" k : "<<k<<endl;
 	// display(arr, l, r);
-	if(pos-l == k-1){
-		cout<<"case 1 "<<endl;
-		printElements(arr, l, pos);
+	if(pos == k){
+		// cout<<"case 1 "<<endl;
+		int pos_index = pos-1+l;//this is the main thing here the position returned from the partition is not the actual index
+		printElements(arr, l, pos_index);
 		return;
 	}
 
-	if(pos-l > k-1){
-		cout<<"case 2 "<<pos-l<<' '<<k-1<<endl;
-		printKSmallest(arr, l, pos-1, k);
+	if(pos > k){
+		// cout<<"case 2 "<<pos-l<<' '<<k-1<<endl;
+		int pos_index = pos-1+l;
+		printKSmallest(arr, l, pos_index-1, k);
 		return;
 	}
 
-	if(pos-l < k-1){
-		cout<<"case 3 "<<pos-l<<' '<<k-1<<endl;
-		printKSmallest(arr, pos+1, r, k-pos-1);
+	if(pos < k){
+		// cout<<"case 3 "<<pos-l<<' '<<k-1<<endl;
+		int pos_index = pos-1+l;
+		//we have now removed position number of elements from the array
+		printKSmallest(arr, pos_index+1, r, k-pos);
 		return;
 	}
 
@@ -69,7 +75,7 @@ int main(){
 	int arr[] = {5, 6, 3, 2, 1, 0, 10, 9, 8};
 	int n = 9;
 
-	int k = 6;
+	int k = 9;
 	printKSmallest(arr, 0, n-1, k);
 
 	return 0;
